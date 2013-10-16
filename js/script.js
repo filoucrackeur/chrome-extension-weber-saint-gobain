3335d8bc91e203e9d996727d183e40a807baedf2
Philippe COURT <philippe.court@gmail.com>
2013-10-16 22:59:22 +0200

    Petit nettoyage et correctif traduction

--- js/script.js
+++ js/script.js
@@ -1,21 +1,11 @@
 (function($) {
 
 	var urlWeber = { "url" : "http://www.e-weber.com/" };
-
-//	if (window.navigator.language === "fr"){
-//		urlWeber = { "url" : "http://www.e-weber.com/fr" };
-//	}
-	
-	var rootUrlWeber = "http://www.e-weber.com/";
 	
 	$(document).ready(function(){
 		getWeberFlags( urlWeber );
 		$('.box').fadeIn('slow');
 		$('.box2').slideDown('slow');
-		
-		$('#logo').on('click',function(){
-			$('.box2').toggle('slow');
-		});
 	});
 	
 	// Recuperation de la liste des drapeaux
@@ -30,10 +20,10 @@
 			$("div#linksByFlags").append(items);
 			$('p').remove();
 			$('#linksByFlags>ul>li>img').each(function(){
-				$(this).attr('src', rootUrlWeber + $(this).attr('src'));
+				$(this).attr('src', urlWeber.url + $(this).attr('src'));
 			});
 			$('#linksByFlags>ul>li>a').each(function(){
-				$(this).attr('href', rootUrlWeber+$(this).attr('href'));
+				$(this).attr('href', urlWeber.url+$(this).attr('href'));
 			});
 		});
 	}
